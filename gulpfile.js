c3c84c27d9747abbfefccc08052c98d13855e888
Dmitry1964 <nosdm64@gmail.com>
2022-04-02 19:21:29 +0300

    added catalog desktop & tablet

--- gulpfile.js
+++ gulpfile.js
@@ -56,8 +56,7 @@ const sprite = () => {
   return gulp.src('source/img/sprite/*.svg')
     .pipe(svgstore({ inlineSvg: true }))
     .pipe(rename('sprite_auto.svg'))
-    .pipe(gulp.dest('build/img'))
-    .pipe(gulp.dest('source/img'));
+    .pipe(gulp.dest('build/img'));
 };
 
 const copySvg = () => {
