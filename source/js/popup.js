d123193e47ab742e4005becfb9d8ef1bbd5104c6
Dmitry1964 <nosdm64@gmail.com>
2022-04-12 20:36:24 +0300

    added validity form

--- source/js/popup.js
+++ source/js/popup.js
@@ -16,7 +16,32 @@ const onButtonCloseHendler = () => {
 };
 
 const onInputChange = () => {
-  console.log(userPhone.value);
+  const newArr = [];
+  const arr = userPhone.value.split('');
+  userPhone.setCustomValidity('');
+  if (arr[0] !== '+') {
+    userPhone.setCustomValidity('Номер телефона должен начинаться с "+7"');
+  }
+
+  if (arr[1] !== '7') {
+    userPhone.setCustomValidity('Номер телефона должен начинаться с "+7"');
+  }
+
+  if (arr.length === 2) {
+    arr.push('(');
+  }
+
+  if (arr.length === 6) {
+    arr.push(')');
+  }
+
+  if (arr.length > 3 && arr.length < 6) {
+    newArr.push(arr[3]);
+  }
+
+  userPhone.value = arr.join('');
+
+  userPhone.reportValidity();
 };
 
 export { modalButtonClose, pageWrapper, userPhone, initModal, onButtonCloseHendler, onInputChange };
